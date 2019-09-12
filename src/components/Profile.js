041449317a904f39c95c69435d9aeadae2f4b38b
github username <github email address>
2019-09-12 13:32:06 -0400

    current styling complete

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -59,7 +59,7 @@ class Profile extends Component {
     return (
       <div className="yellow-stripes-corners-background">
         <div>
-          <h2 className="form-title-profile"> Hey, {this.props.username}! Here are all of your current posts: </h2>
+          <h2 className="form-title-profile"> Hey, {this.props.username}! Here are your current posts: </h2>
           <br></br>
         </div>
         <div>
